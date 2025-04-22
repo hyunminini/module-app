dde18bab1eb010263c3bcceb33af8ca40b33df4c
김현민 <hyunmins@douzone.com>
2025-04-22 20:45:08 +0900

    no message

---
+++ .history/src/modules/messenger/screens/RoomListView_20250422204400.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const RoomListView = () => {
+    return (
+        <View>
+            <Text>HI</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default RoomListView;
